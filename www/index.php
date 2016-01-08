9d05cd7c416c43c1e8f5d7adf9f5a48c21c1cae2
rsparapa <rsparapa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-01-08 17:54:42 +0000

    move
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survbart@9 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,7 +43,7 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> <a href="../survbart_1.0.tar.gz">survbart_1.0.tar.gz</a> </p>
+<p> <a href="survbart_1.0.tar.gz">survbart_1.0.tar.gz</a> </p>
 
 <p> The <strong>project summary page</strong> you can find 
 <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
